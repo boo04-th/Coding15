33312f7abca7a11f9a2fc021054c6ec636e7163a
Hient <hient@usf.edu>
2025-03-15 19:49:20 -0500

    Task 3 - Removing Risk Items

---
+++ cc_15.js
@@ -0,0 +1,19 @@
+//Task 1: Creating the Base Structure
+const riskDashboard = document.getElementById("riskDashboard");
+console.log("risk dashboard loaded");
+
+function addRiskItem(riskName, riskLevel, department) {
+    const riskCard = document.createElement("div");
+    riskCard.className = "riskCard";
+
+    updateRiskCardStyle(riskCard, riskLevel);
+    
+    riskCard.innerHTML = `
+    <p><strong>risk name:</strong>${riskName}</p>
+    <p><strong>risk level:</strong><span class="riskLevelText">${riskLevel}</span></p>
+    <p><strong>department:</strong>${department}</p>
+    <button class="resolveButton">resolve</button>
+    `;
+//Task 3: Removing Risk Items
+const resolveButton = riskCard.querySelector(".resolveButton");
+resolveButton.addEventListener("click", function (event) {
